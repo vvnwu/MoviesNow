741da1b02cd80a8f00c8f72323104303f5e793f6
wuvvn <wu.vivian00@gmail.com>
2019-04-03 23:46:27 -0700

    changes to json

--- lab8/core/data.js
+++ lab8/core/data.js
@@ -1,14 +1,16 @@
 let someData = "<ul><li>Beep</li><li>Boop</li><li>Wednesday</li><li>Thursday</li><li>Friday</li><li>Saturday</li><li>Sunday</li></ul>";
 
-let someOtherData = ['Avengers','Adrift in space with no food or water, Tony Stark sends a message to Pepper Potts as his oxygen supply starts to dwindle. Meanwhile, the remaining Avengers -- Thor, Black Widow, Captain America and Bruce Banner -- must figure out a way to bring back their vanquished allies for an epic showdown with Thanos -- the evil demigod who decimated the planet and the universe.','Joss Whedon, Tony Stark','Cineplex Odeon']//= movie1//[movie1, "hello"]//["Avengers", "Avengers", "Bavengers", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
 
-var movie1 = ['Avengers','Adrift in space with no food or water, Tony Stark sends a message to Pepper Potts as his oxygen supply starts to dwindle. Meanwhile, the remaining Avengers -- Thor, Black Widow, Captain America and Bruce Banner -- must figure out a way to bring back their vanquished allies for an epic showdown with Thanos -- the evil demigod who decimated the planet and the universe.','Joss Whedon, Tony Stark','Cineplex Odeon']
-//let movie1 = {
-//    'title' : 'Avengers',
-//    'description' : 'Adrift in space with no food or water, Tony Stark sends a message to Pepper Potts as his oxygen supply starts to dwindle. Meanwhile, the remaining Avengers -- Thor, Black Widow, Captain America and Bruce Banner -- must figure out a way to bring back their vanquished allies for an epic showdown with Thanos -- the evil demigod who decimated the planet and the universe.',
-//    'cast' : 'Joss Whedon, Tony Stark',
-//    'theater' : 'Cineplex Odeon'
-//}
+//let someOtherData = ['Avengers','Adrift in space with no food or water, Tony Stark sends a message to Pepper Potts as his oxygen supply starts to dwindle. Meanwhile, the remaining Avengers -- Thor, Black Widow, Captain America and Bruce Banner -- must figure out a way to bring back their vanquished allies for an epic showdown with Thanos -- the evil demigod who decimated the planet and the universe.','Joss Whedon', 'Tony Stark','Cineplex Odeon', 'Showing tonight at 3:30, 5:30, 7:30, and 10:00']//= movie1//[movie1, "hello"]//["Avengers", "Avengers", "Bavengers", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
+
+//var movie1 = ['Avengers','Adrift in space with no food or water, Tony Stark sends a message to Pepper Potts as his oxygen supply starts to dwindle. Meanwhile, the remaining Avengers -- Thor, Black Widow, Captain America and Bruce Banner -- must figure out a way to bring back their vanquished allies for an epic showdown with Thanos -- the evil demigod who decimated the planet and the universe.','Joss Whedon, Tony Stark','Cineplex Odeon']
+var movie1 = {
+    'title' : 'Avengers',
+    'description' : 'Adrift in space with no food or water, Tony Stark sends a message to Pepper Potts as his oxygen supply starts to dwindle. Meanwhile, the remaining Avengers -- Thor, Black Widow, Captain America and Bruce Banner -- must figure out a way to bring back their vanquished allies for an epic showdown with Thanos -- the evil demigod who decimated the planet and the universe.',
+    'cast' : 'Joss Whedon, Tony Stark',
+    'theater' : 'Cineplex Odeon'
+}
+let movieArray = [movie1];
 module.exports = {
   getHTML: function () {
       console.log("called: getHTML");
@@ -18,7 +20,7 @@ module.exports = {
   getJSON: function () {
       console.log("called: getJSON");
       // Note: this could be from a DB, for now it's just hard-coded
-      return someOtherData;
+      return movieArray;
   }
 };
 
